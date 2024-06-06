e7062114e19c87b8130726221e743af1b56f4a18
Hilde-otieno <meghanadhiambootieno@gmail.com>
2024-06-06 19:10:37 +0300

    created files

--- animal.py
+++ animal.py
@@ -1,23 +1,42 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
-    def move(self):
-        pass
-    def make_noise(self):
-        pass
-        pass
-class Cat(Animal):
-    def move(self):
-        print("The cat is walking")
-    def make_noise(self):
-        print("meow")
-class Bird(Animal):
-    def move(self):
-        print("The bird is flying")
-    def make_noise(self):
-        print("chirp")
-class Fish(Animal):
-    def move(self):
-        print("The fish is swimming")
-    def make_noise(self):
-        print("boops")
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+#     def move(self):
+#         pass
+#     def make_noise(self):
+#         pass
+#         pass
+# class Cat(Animal):
+#     def move(self):
+#         print("The cat is walking")
+#     def make_noise(self):
+#         print("meow")
+# class Bird(Animal):
+#     def move(self):
+#         print("The bird is flying")
+#     def make_noise(self):
+#         print("chirp")
+# class Fish(Animal):
+#     def move(self):
+#         print("The fish is swimming")
+#     def make_noise(self):
+#         print("boops")
+
+class Design:
+      def __init__(self,mood,temperature):
+           self.mood = mood
+           self.temperature = temperature
+
+      def check_design(self,mood,temperature):
+            if temperature <= 15 and mood =="Sad":
+                  return "dark ankara patterns"
+            elif temperature >15 and temperature<30 and mood =="Neutal":
+                  return "circular ankara patterns"
+            elif temperature >30 and mood =="Happy":
+                  return "vibrant ankara patterns"
+            else:
+                  return "Dont change the designs on he ankara"
+
+temperature = 22
+mood = "Happy"  
+    
